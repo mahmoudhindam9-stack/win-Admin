07428e48602df9d716766e6dec4b7af5fec448be
mahmoudhindam9-stack <mahmoudhindam9@gmail.com>
2026-09-04 18:12:19 +0300

    feat: expose updater IPC to renderer

--- electron/preload.cjs
+++ electron/preload.cjs
@@ -9,5 +9,14 @@ contextBridge.exposeInMainWorld('electronAPI', {
     ipcRenderer.on('execution-progress', handler);
     return () => ipcRenderer.removeListener('execution-progress', handler);
   },
-  routerApi: (action, data) => ipcRenderer.invoke('router-api', action, data)
+  routerApi: (action, data) => ipcRenderer.invoke('router-api', action, data),
+  checkForUpdate: () => ipcRenderer.invoke('update-check'),
+  downloadUpdate: () => ipcRenderer.invoke('update-download'),
+  installUpdate: () => ipcRenderer.invoke('update-install'),
+  getAppVersion: () => ipcRenderer.invoke('app-version'),
+  onUpdateStatus: (callback) => {
+    const handler = (event, data) => callback(data);
+    ipcRenderer.on('update-status', handler);
+    return () => ipcRenderer.removeListener('update-status', handler);
+  }
 });
